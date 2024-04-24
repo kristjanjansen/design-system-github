b2faf75b3c9fe8c38cf748c61a37754579e1fddf
Kristjan Jansen <kristjan.jansen@gmail.com>
2024-04-24 21:24:10 +0300

    update

--- ComparisonCard/ComparisonCardFooter/ComparisonCardFooter.d.ts
+++ ComparisonCard/ComparisonCardFooter/ComparisonCardFooter.d.ts
@@ -1,5 +1,6 @@
 import React from 'react';
 export interface ComparisonCardFooterProps {
     children: React.ReactNode;
+    className?: string;
 }
 export declare const ComparisonCardFooter: React.FC<ComparisonCardFooterProps>;
