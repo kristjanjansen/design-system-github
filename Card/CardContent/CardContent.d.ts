024f4d920d1fc481424d02abaf8593d00a68d0a6
Kristjan Jansen <kristjan.jansen@gmail.com>
2024-04-26 10:06:22 +0300

    update

--- Card/CardContent/CardContent.d.ts
+++ Card/CardContent/CardContent.d.ts
@@ -3,5 +3,6 @@ export interface CardContentProps {
     children: React.ReactNode;
     align?: 'left' | 'center' | 'right';
     gap?: 'small' | 'medium';
+    className?: string;
 }
 export declare const CardContent: React.FC<CardContentProps>;
