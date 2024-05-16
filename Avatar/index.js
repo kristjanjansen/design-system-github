55c790a100184bdab5b3b53fdcceb516742be2d9
Kristjan Jansen <kristjan.jansen@gmail.com>
2024-05-16 08:09:02 +0300

    Updates

--- Avatar/index.js
+++ Avatar/index.js
@@ -1,2 +1,2 @@
-"use client";import e from"react";var r="base__YNkBW__0-1-38";const t=({children:t})=>e.createElement("div",{className:r},t);export{t as Avatar};
+"use client";import e from"react";var r="base__YNkBW__0-1-40";const t=({children:t})=>e.createElement("div",{className:r},t);export{t as Avatar};
 //# sourceMappingURL=index.js.map
--- Breadcrumbs/Breadcrumbs.d.ts
+++ Breadcrumbs/Breadcrumbs.d.ts
@@ -3,6 +3,7 @@ import { AriaBreadcrumbsProps } from 'react-aria';
 export interface BreadcrumbsProps extends AriaBreadcrumbsProps {
     className?: string;
     children: ReactElement | ReactElement[];
+    inverted?: boolean;
 }
 declare const Breadcrumbs: (props: BreadcrumbsProps) => React.JSX.Element;
 export default Breadcrumbs;
